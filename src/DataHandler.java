993f09cd46b72b8ac312ceba8395c1f7fca8408f
stefan4472 <stefan.kussmaul@gmail.com>
2015-06-07 13:04:31 -0400

    Added Numerous Helper Functions to DataHandler and Goal, Fixes+Improvements

--- src/DataHandler.java
+++ src/DataHandler.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /* This class handles reading and writing data to the program's data file
  * A DataHandler is initialized with the name of the local file that stores data
@@ -70,12 +71,11 @@ public class DataHandler {
     public ArrayList<Session> getSessions() {
         ArrayList<Session> stored_sessions = new ArrayList<Session>();
         ArrayList<String> data = getData();
-        for (int i = 0; i < data.size(); i++) {
+        for (int i = data.indexOf("Log:") + 1; i < data.size(); i++) {
             if (data.get(i).equals("Log:")) { /* start of logs */
                 try {
                     stored_sessions.add(new Session(data.get(i)));
-                } catch (Exception e) {
-                } /* just ignore corrupt data */
+                } catch (Exception e) {} /* just ignore corrupt data */
             }
         }
         for (int i = 1; i < stored_sessions.size(); i++) {
@@ -95,19 +95,26 @@ public class DataHandler {
         }
         return stored_sessions;
     }
-    
+    /* reads datafile and returns ArrayList of Goals */
+    public ArrayList<Goal> getGoals() {
+        ArrayList<Goal> stored_goals = new ArrayList<Goal>();
+        ArrayList<String> data = getData();
+        for(int i = data.indexOf("Goals:") + 1; !data.get(i).equals("Log:") ; i++) {
+            try {
+                stored_goals.add(new Goal(data.get(i)));
+            } catch(Exception e) {} /* ignore corrupted data */
+        }
+        return stored_goals;
+    }
     /* reads datafile and returns ArrayList of names of activities */
     public ArrayList<String> getActivities() {
         ArrayList<String> stored_activities = new ArrayList<String>();
         ArrayList<String> data = getData();
-        for (int i = 0; i < data.size(); i++) {
-            if (data.get(i).equals("Activities:")) { /* start of activities */
-                stored_activities.add(data.get(i));
-            }
+        for (int i = data.indexOf("Activities:"); !data.get(i).equals("Goals:"); i++) {
+            stored_activities.add(data.get(i));
         }
         return stored_activities;
     }
-    // todo: getGoals
     /* reads log and returns ArrayList of the num_sessions most recent sessions */
     public ArrayList<Session> getMostRecentSessions(int num_sessions) {
         /* get stored sessions from log, which are stored from newest to oldest */
@@ -117,18 +124,53 @@ public class DataHandler {
             recent_sessions.add(stored_sessions.get(i));
         return recent_sessions;
     }
-
+    /* reads datafile and returns ArrayList of goals that are currently relevant
+     * THIS DOES NOT FILTER OUT GOALS THAT HAVE BEEN COMPLETED */
+    public ArrayList<Goal> getCurrentGoals() {
+        ArrayList<Goal> stored_goals = getGoals();
+        /* get current time in milliseconds */
+        long current_time = Calendar.getInstance().get(Calendar.MILLISECOND);
+        for(int i = 0; i < stored_goals.size(); i++) {
+            /* remove any goals that aren't relevant at current System time */
+            if(!stored_goals.get(i).isRelevant(current_time))
+                stored_goals.remove(i);
+        }
+        return stored_goals;
+    }
     /* adds session to log, by default as first session after "Logs:" */
     public boolean addSession(Session add) {
         ArrayList<String> data = getData();
         data.add(data.indexOf("Logs:") + 1, add.toString());
         return writeData(data);
     }
-
     /* adds activity to datafile under "Activities:" header */
     public boolean addActivity(String activity_name) {
         ArrayList<String> data = getData();
         data.add(data.indexOf("Activities") + 1, activity_name);
+        return writeData(data);
+    }
+    /* removes activity from datafile */
+    public boolean removeActivity(String activity_name) {
+        ArrayList<String> data = getData();
+        for(int i = data.indexOf("Activities:"); !data.get(i).equals("Goals:"); i++) {
+            if(data.get(i).equals(activity_name))
+                data.remove(i);
+        }
+        return writeData(data);
+    }
+    /* adds goal to datafile under "Goals:" header */
+    public boolean addGoal(Goal add) {
+        ArrayList<String> data = getData();
+        data.add(data.indexOf("Goals:") + 1, add.toString());
+        return writeData(data);
+    }
+    /* removes goal from datafile */
+    public boolean removeGoal(Goal remove) {
+        ArrayList<String> data = getData();
+        for(int i = data.indexOf("Goals:"); !data.get(i).equals("Log:"); i++) {
+            if(data.get(i).equals(remove.toString()))
+                data.remove(i);
+        }
         return writeData(data);
     }
 }
--- src/Goal.java
+++ src/Goal.java
@@ -9,38 +9,54 @@ import java.util.Calendar;
  * Goals are constructed using a String taken from the
  * program's datafile, which holds all necessary information */
 public class Goal {
+    private String file_name = ""; /* name of local file where program data can be retrieved */
     private String activity = "";
     private int goal_sessions = 0;
     private long goal_time = 0;
-    private Calendar start_time = Calendar.getInstance();
+    private long start_time = 0;
     private long time_limit = 0; /* timelimit stored in milliseconds */
     private boolean recurring = false;
     /* initializes Goal using stored data */
-    public Goal(String constructor) {
+    public Goal(String constructor) throws Exception {
         /* parse constructor into individual values */
         String[] data = constructor.split(","); // comma escaped?
         activity = data[0];
         goal_sessions = Integer.parseInt(data[1]);
         goal_time = Long.parseLong(data[2]);
-        start_time.setTimeInMillis(Long.parseLong(data[3]));
+        start_time = Long.parseLong(data[3]);
         time_limit = Long.parseLong(data[4]);
         recurring = Boolean.parseBoolean(data[5]);
     }
-    /* returns a String representation of Goal, for data handling */
+    /* returns whether Goal has a time limit, in which case long time_limit != 0 */
+    public boolean hasTimeLimit() { return time_limit != 0; }
+    /* returns whether Goal has been completed by checking if goal progress equals
+     * 1, or 100 percent */
+    public boolean isComplete() { return getProgress()[1] == 1; }
+    /* returns whether Goal is relevant at specified time
+     * if a time limit is specified it will see if System time falls within time limit
+     * if no time limit is specified it is relevant */
+    public boolean isRelevant(long at_time) {
+        if(hasTimeLimit()) {
+            return (at_time >= start_time && at_time <= start_time + time_limit);
+        } else {
+            return true;
+        }
+    }
+     /* returns a String representation of Goal, for data handling */
     @Override public String toString() {
-        return activity + "," + goal_sessions + "," + goal_time + "," + start_time.getTimeInMillis() +
+        return activity + "," + goal_sessions + "," + goal_time + "," + start_time +
                 "," + time_limit + "," + recurring;
     }
     /* accesses datahandler and returns an ArrayList of Sessions
     that are relevant towards Goal progress */
-    public ArrayList<Session> getRelevantSessions(String file_name) {
+    public ArrayList<Session> getRelevantSessions() {
         DataHandler data_handler = new DataHandler(file_name);
         ArrayList<Session> data = new ArrayList<Session>();
         if (data_handler.isValid()) { // todo: what if it isn't?
             data = data_handler.getSessions();
              /* we need to remove any Sessions that aren't relevant to the goal */
             if (time_limit != 0) { /* a goal_time limit was specified */
-                long end_time = start_time.getTimeInMillis() + time_limit;
+                long end_time = start_time + time_limit;
                 for (int i = 0; i < data.size(); i++) {
                     if (data.get(i).getDate().before(start_time)
                             || data.get(i).getDate().after(end_time))
@@ -56,21 +72,23 @@ public class Goal {
         }
         return data;
     }
-    /* returns an int[] where first element is goal_time progress in
-     * milliseconds and second is percent progress
-     * file_name is the name of the local file where all data is
-     * stored */
-     public int[] getProgress(String file_name) {
-         int[] progress = new int[] {0, 0};
-         ArrayList<Session> data = getRelevantSessions(file_name);
+    /* returns a long[] where first element is "raw" progress (i.e. milliseconds
+     * practiced or sessions practiced) and second is percent progress */
+     public long[] getProgress() {
+         long[] progress = new long[] {0, 0};
+         ArrayList<Session> data = getRelevantSessions();
          /* now calculate relevant progress */
          if(goal_time != 0) { /* goal time specified */
              long time_spent = 0;
              for (int i = 0; i < data.size(); i++)
                  time_spent += data.get(i).getTime();
+             /* total time spent, in milliseconds */
+             progress[0] = time_spent;
             /* calculated as total time spent during relevant sessions divided by target time */
-             progress[0] = (int) (time_spent / goal_time);
+             progress[1] = time_spent / goal_time;
          } else if(goal_sessions != 0) { /* goal sessions specified */
+             /* total practice sessions */
+             progress[0] = data.size();
              /* calculated as number of relevant sessions recorded divided
              by target sessions  */
              progress[1] = data.size() / goal_sessions;
